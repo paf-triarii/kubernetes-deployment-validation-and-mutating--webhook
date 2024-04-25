fb036edff455ad3f25927061377d7490c92d5b67
PAF Triarii <pedroarias1015@gmail.com>
2024-04-25 13:54:49 +0200

    Extending deployment model definition

--- m_models/deployments.py
+++ m_models/deployments.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel, Field
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Union
 from m_models.commons import Metadata
 
 class MatchLabels(BaseModel):
-    app: str
+    labels: Dict[str, str] = Field(default_factory=dict)
 
 class Selector(BaseModel):
     matchLabels: MatchLabels
@@ -24,13 +24,95 @@ class Container(BaseModel):
     terminationMessagePath: Optional[str] = None
     terminationMessagePolicy: Optional[str] = None
 
+class LabelSelectorRequirement(BaseModel):
+    key: str
+    operator: str
+    values: Optional[List[str]]
+
+class LabelSelector(BaseModel):
+    matchLabels: Optional[Dict[str, str]]
+    matchExpressions: Optional[List[LabelSelectorRequirement]] = None
+
+class AffinityTerm(BaseModel):
+    labelSelector: LabelSelector
+    topologyKey: str
+
+class PodAffinity(BaseModel):
+    preferredDuringSchedulingIgnoredDuringExecution: Optional[List[Dict[str, Union[AffinityTerm, int]]]] = None
+
+
+class Affinity(BaseModel):
+    podAntiAffinity: Optional[PodAffinity] = None
+
+class ProbeHTTPGet(BaseModel):
+    path: str
+    port: Union[int, str]
+    scheme: str
+
+class Probe(BaseModel):
+    failureThreshold: int
+    httpGet: ProbeHTTPGet
+    initialDelaySeconds: int
+    periodSeconds: int
+    successThreshold: int
+    timeoutSeconds: int
+
+class Port(BaseModel):
+    containerPort: int
+    name: str
+    protocol: str = 'TCP'
+
+class VolumeMount(BaseModel):
+    mountPath: str
+    name: str
+    readOnly: Optional[bool] = True
+
+class VolumeConfigMapItem(BaseModel):
+    key: str
+    path: str
+
+class VolumeConfigMap(BaseModel):
+    name: str
+    defaultMode: Optional[int] = '0600'
+    items: Optional[List[VolumeConfigMapItem]] = None
+
+class Volume(BaseModel):
+    name: str
+    configMap: VolumeConfigMap = None
+
+class Toleration(BaseModel):
+    key: Optional[str] = None
+    operator: Optional[str] = None
+    effect: Optional[str] = None
+
+class Container(BaseModel):
+    name: str
+    image: str
+    args: Optional[List[str]] = None
+    imagePullPolicy: Optional[str] = None
+    ports: Optional[List[Port]] = None
+    livenessProbe: Optional[Probe] = None
+    readinessProbe: Optional[Probe] = None
+    resources: Optional[dict] = None
+    securityContext: Optional[dict] = None
+    terminationMessagePath: Optional[str] = None
+    terminationMessagePolicy: Optional[str] = None
+    volumeMounts: Optional[List[VolumeMount]] = None
+
 class TemplateSpec(BaseModel):
     containers: List[Container]
+    affinity: Optional[Affinity] = None
     dnsPolicy: Optional[str] = None
+    nodeSelector: Optional[Dict[str, str]] = None
+    priorityClassName: Optional[str] = None
     restartPolicy: Optional[str] = None
     schedulerName: Optional[str] = None
     securityContext: Optional[dict] = None
+    serviceAccount: Optional[str] = None
+    serviceAccountName: Optional[str] = None
     terminationGracePeriodSeconds: Optional[int] = None
+    tolerations: Optional[List[Toleration]] = None
+    volumes: Optional[List[Volume]] = None
 
 class TemplateMetadata(BaseModel):
     creationTimestamp: Optional[str]
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 kubectl create deployment nginx --image=nginx --dry-run=client -o json > nginx-deployment.json
-curl -X POST -H "Content-Type: application/json" -d @nginx-deployment.json http://127.0.0.1:8000/validate
+curl -X POST -H "Content-Type: application/json" -d @nginx-deployment.json http://127.0.0.1:8000/validate | jq .
